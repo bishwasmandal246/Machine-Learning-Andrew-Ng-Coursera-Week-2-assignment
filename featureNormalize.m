8c72a63a7be2ae76a5e558e2b626e1133ab1b721
Bishwas Mandal <bishwasmandal246@gmail.com>
2019-03-31 02:16:54 +0530

    Create featureNormalize.m

---
+++ featureNormalize.m
@@ -0,0 +1,13 @@
+function [X_norm, mu, sigma] = featureNormalize(X)
+  
+  X_norm = X;
+  mu = zeros(1, size(X, 2));
+  sigma = zeros(1, size(X, 2));
+  
+      
+  
+  mu = mean(X);
+  sigma = std(X);
+  X_norm = (X - mu)./sigma;
+  
+end
